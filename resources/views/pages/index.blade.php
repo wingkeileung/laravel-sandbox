53fd64f78f179c0169c41edae249c98b49dd4d40
Wing Kei Leung <winky.leung@gmail.com>
2018-03-08 22:18:22 -0800

    Playing with basic routing and controllers

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{config('app.name', 'LSAPP')}}</title>
+  </head>
+  <body>
+    <h1>Welcome to Laravel</h1>
+    <p>This is the laravel application sandbox</p>
+  </body>
+</html>
