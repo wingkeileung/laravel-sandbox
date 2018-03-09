51d34e6223f6652457f7b58d74358e5fb0b05716
Wing Kei Leung <winky.leung@gmail.com>
2018-03-09 08:57:47 -0800

    Create blade template and basic styling with bootstrap 4.0

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,13 +1,9 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{config('app.name', 'LSAPP')}}</title>
-  </head>
-  <body>
-    <h1>Welcome to Laravel</h1>
+@extends('layouts.app')
+
+@section('content')
+  <div class="jumbotron text-center">
+    <h1>{{$title}}</h1>
     <p>This is the laravel application sandbox</p>
-  </body>
-</html>
+    <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a><a class="btn btn-success btn-lg ml-2" href="/register" role="button">Register</a></p>
+  </div>
+@endsection
--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -1,13 +1,12 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{config('app.name', 'LSAPP')}}</title>
-  </head>
-  <body>
-    <h1>Services</h1>
-    <p>This is the services page</p>
-  </body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+  <h1>{{$title}}</h1>
+  @if(count($services) > 0)
+    <ul class="list-group">
+      @foreach($services as $service)
+        <li class="list-group-item">{{$service}}</li>
+      @endforeach
+    </ul>        
+  @endif
+@endsection
